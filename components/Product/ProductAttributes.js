ee28cb3c3c87f1bfe55bbe09d4adfdfab4f0a23b
archanshahh <archanshahh@gmail.com>
2020-05-07 13:28:01 -0400

    logout done

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,49 +1,59 @@
-import {Header,Button,Modal} from 'semantic-ui-react';
+import { Header, Button, Modal } from 'semantic-ui-react';
 import React from 'react';
 import axios from 'axios';
 import baseUrl from '../../utils/baseUrl';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
-function ProductAttributes({description,_id}) {
+function ProductAttributes({ description, _id, user }) {
   const [modal, setModal] = React.useState(false)
   const router = useRouter();
 
-  async function handleDelete(){
-      const url = `${baseUrl}/api/product`
-      const payload = {params:{_id}}
+  const isRoot = user && user.role === 'root';
+  const isAdmin = user && user.role === 'admin';
 
-      await axios.delete(url,payload);
-      router.push('/');
-    }
+  const isRootOrAdmin = isRoot || isAdmin;
+
+  async function handleDelete() {
+    const url = `${baseUrl}/api/product`
+    const payload = { params: { _id } }
+
+    await axios.delete(url, payload);
+    router.push('/');
+  }
 
   return <>
 
     <Header as="h3">About this product</Header>
     <p>{description}</p>
-    <Button
-      icon="trash alternate outline"
-      color="red"
-      content="Delete Product"
-      onClick={()=>setModal(true)} 
-    />
+    {isRootOrAdmin && (
+      <>
+        <Button
+          icon="trash alternate outline"
+          color="red"
+          content="Delete Product"
+          onClick={() => setModal(true)}
+        />
 
-    <Modal open={modal} dimmer="blurring">
-      <Modal.Header>Confirm Delete</Modal.Header>
-      <Modal.Content>
-        Are you sure you want to delete this product?
-      </Modal.Content>
-      <Modal.Actions>
-        <Button content="Cancel"
-        onClick={()=>setModal(false)}
-        />
-        <Button 
-        negative icon="trash" 
-        labelPosition="right" 
-        content="Delete"
-        onClick={handleDelete}
-        />
-      </Modal.Actions>
-    </Modal>
+        <Modal open={modal} dimmer="blurring">
+          <Modal.Header>Confirm Delete</Modal.Header>
+          <Modal.Content>
+            Are you sure you want to delete this product?
+        </Modal.Content>
+          <Modal.Actions>
+            <Button content="Cancel"
+              onClick={() => setModal(false)}
+            />
+            <Button
+              negative icon="trash"
+              labelPosition="right"
+              content="Delete"
+              onClick={handleDelete}
+            />
+          </Modal.Actions>
+        </Modal>
+      </>
+    )}
+
   </>;
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -25,8 +25,20 @@ class MyApp extends App {
       try {
         const payload = { headers: { Authorization: token } };
         const url = `${baseUrl}/api/account`;
+        
         const response = await axios.get(url, payload);
         const user = response.data;
+        
+        const isRoot = user.role === 'root';
+        const isAdmin = user.role === 'admin';
+
+        //if not admin or root, redirect from '/create' page
+        const isNotPermitted = !(isRoot || isAdmin) && ctx.pathname === '/create';
+
+        if(isNotPermitted){
+          redirectUser(ctx,'/')
+        }
+
         pageProps.user = user;
       } catch (error) {
         console.error("Error getting current user", error);
