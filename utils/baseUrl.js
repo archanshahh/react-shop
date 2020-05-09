0f1452ecf6f5a9358d67e02cf6662c150f541f3c
archanshahh <archanshahh@gmail.com>
2020-05-09 03:48:11 -0400

    deployment v10

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 
-const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://reactshopp.herokuapp.com' 
-: 'http://localhost:3000';
+// const baseUrl = process.env.NODE_ENV === "production" 
+// ? 'http://reactshopp.herokuapp.com' 
+// : 'http://localhost:3000';
 
+const baseUrl = 'http://reactshopp.herokuapp.com' ;
 export default baseUrl;
