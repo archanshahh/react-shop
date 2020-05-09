9651680ae2c3570e994268e4a0991534c8969c08
archanshahh <archanshahh@gmail.com>
2020-05-09 00:27:04 -0400

    handling delete products

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,18 +1,19 @@
 import Product from '../../models/Product';
+import Cart from '../../models/Cart';
 import connectDb from '../../utils/connectDb';
 
-connectDb(); 
+connectDb();
 
-export default async(req,res) => {
-    switch(req.method){
+export default async (req, res) => {
+    switch (req.method) {
         case "GET":
-            await handleGetRequest(req,res);
+            await handleGetRequest(req, res);
             break;
         case "DELETE":
-            await handleDeleteRequest(req,res);
+            await handleDeleteRequest(req, res);
             break;
         case "POST":
-            await hanadlePostRequest(req,res);
+            await hanadlePostRequest(req, res);
             break;
         default:
             res.status(405).send(`Method ${req.method} not allowed`)
@@ -20,12 +21,12 @@ export default async(req,res) => {
     }
 }
 
-async function hanadlePostRequest(req,res){
-    const {name,price,description,mediaUrl} = req.body
-    
+async function hanadlePostRequest(req, res) {
+    const { name, price, description, mediaUrl } = req.body
+
     try {
-        if(!name || !price || !description || !mediaUrl){
-            return res.status(422).send("Product missing one or more fields");        
+        if (!name || !price || !description || !mediaUrl) {
+            return res.status(422).send("Product missing one or more fields");
         }
         const product = await new Product({
             name,
@@ -33,24 +34,39 @@ async function hanadlePostRequest(req,res){
             description,
             mediaUrl
         }).save();
-    
+
         res.status(201).json(product);
     } catch (error) {
         console.error(error);
         res.status(500).send("Server error in creating product");
     }
-    
+
 }
 
-async function handleGetRequest(req,res){
-    const {_id} = req.query
-    const product = await Product.findOne({_id : _id})
+async function handleGetRequest(req, res) {
+    const { _id } = req.query
+    const product = await Product.findOne({ _id: _id })
     res.status(200).json(product);
 
 }
 
-async function handleDeleteRequest(req,res){
-    const {_id} = req.query
-    await Product.findOneAndDelete({_id : _id})
-    res.status(204).json({});
+async function handleDeleteRequest(req, res) {
+    const { _id } = req.query
+    try {
+        //delete product by id
+        await Product.findOneAndDelete({ _id});
+
+        //remove product from all carts
+        await Cart.updateMany(
+            {"products.product":_id},
+            { $pull:{products:{product:_id}}}
+        );
+        
+        res.status(204).json({});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting product');
+
+    }
+
 }
